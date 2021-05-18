99559c1cd1415804ae4c6f4a1d92e4149dc09435
Andrey Medvedev <andreygasparyan539@gmail.com>
2021-05-18 22:08:06 +0300

    Complete homework lesson 3

---
+++ src/JavaLesson3/Main.java
@@ -0,0 +1,11 @@
+package JavaLesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        // String uberflu?; // No we can`t create it.
+        String _Identifier;
+        // String \u006fIdentifier; // No we can`t create it.
+        // String &myVar; // No we can`t create it.
+        String myVariab1le;
+    }
+}
